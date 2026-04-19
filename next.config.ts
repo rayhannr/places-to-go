1e34b2c201747d9059eb6478cd31db3add1bbe21
rayhannr <rayhannr07@gmail.com>
2026-04-19 22:24:36 +0700

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['grammy']
-  }
+  serverExternalPackages: ['grammy']
 }
 
 export default nextConfig
